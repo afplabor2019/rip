22caaf2fd4c28a9f781643bc608e87bfca899c86
alexrace <50375493+alexrace@users.noreply.github.com>
2019-09-30 16:50:15 +0200

    Form1.cs
    Elb*sztam a form1 előző verzióját

--- Kis projekt/program/afp/afp/Form1.cs
+++ Kis projekt/program/afp/afp/Form1.cs
@@ -73,21 +73,7 @@ namespace afp
             }
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            DataTable dt = new DataTable();
-            dt.Columns.Add(new DataColumn("colBestBefore", typeof(DateTime)));
-            dt.Columns.Add(new DataColumn("colStatus", typeof(string)));
-
-            dt.Columns["colStatus"].Expression = String.Format("IIF(colBestBefore < #{0}#, 'Ok','Not ok')", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
-
-            dt.Rows.Add(DateTime.Now.AddDays(-1));
-            dt.Rows.Add(DateTime.Now.AddDays(1));
-            dt.Rows.Add(DateTime.Now.AddDays(2));
-            dt.Rows.Add(DateTime.Now.AddDays(-2));
-
-            dataGridView1.DataSource = dt;
-        }
+        
 
         
     }
