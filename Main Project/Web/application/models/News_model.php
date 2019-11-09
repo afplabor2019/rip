d7a9f703b1fea76d5021e344b8e83b56a3428777
Burus Bence <burusbence@gmail.com>
2019-11-09 21:55:20 +0100

    created article logic, edited a few configs
    -edited dummy record slug to be url friendly
    -edited config/autoload.php to include url_helper and language
    -edited config/config.php for routing
    -added replacement character in config/foreign_chars.php
    -modified rooting
    -removed an unnecessary line in controller/Auth.php
    -created logic for article (controller, model)
    -removed unnecessary files (Welcome.php, wecome_message.php)
    -added lang files for ion auth
    -edited header
    -edited view for article, news
    -added beginners guide, so others can start working
    -created beginners guide so the other colleagues can get started

--- Main Project/Web/application/models/News_model.php
+++ Main Project/Web/application/models/News_model.php
@@ -8,9 +8,25 @@ class News_model extends CI_Model{
     }
     
     public function get_news(){
-        $this->db->select('active, title, body, create_at,  created_by');
+        $this->db->select('title, body, create_at,  created_by');
         $this->db->from('article');
+        $this->db->where('active = 1');
         $query = $this->db->get();
         return $query->result_array();
     }
+
+    public function get_article($slug){
+        $this->db->select('title, body, create_at,  created_by');
+        $this->db->from('article');
+        $array = array('active' => 1, 'slug' => $slug);
+        $this->db->where($array);
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+
+    /* TODO
+
+        edit querry so created_by will show the author not its id.
+    
+    */
 }
