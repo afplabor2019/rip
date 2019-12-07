49afa0aa56f6155b7374e0395b9d6c50484be60a
Tolvaj Balázs <tolvibalazs@gmail.com>
2019-12-07 15:44:01 +0100

    Header and news views with css
    Tolvaj Balázs, Kiss Milán

--- Main Project/Web/application/views/header.php
+++ Main Project/Web/application/views/header.php
@@ -1,4 +1,19 @@
-<ul>news</ul>
-<ul>travel</ul>
-<ul>sport</ul>
-<ul>login</ul>
+<head>
+<link rel="stylesheet" href="style/css/default.css">
+<!--
+<link rel = "stylesheet" type = "text/css" 
+   href = "<?php echo base_url(); ?>style/css/default.css">
+-->
+</head>
+
+
+<div id="menu-wrapper">
+	<div id="menu" class="container">
+		<ul>
+			<li><a href="#">news</a></li>
+			<li><a href="#">travel</a></li>
+			<li><a href="#">sport</a></li>
+			<li><a href="#">login</a></li>
+        </ul>
+    </div>
+</div>
--- Main Project/Web/application/views/news/news.php
+++ Main Project/Web/application/views/news/news.php
@@ -1,11 +1,30 @@
+<head>
+<link rel = "stylesheet" type = "text/css" 
+   href = "<?php echo base_url(); ?>style/css/default.css">
+</head>
 <html>
 <body>
-<h1><?=($items[0]['title']);?></h1>
-<p><?=($items[0]['body']);?></p>
-<p><?=($items[0]['created_at']);?></p>
-<p><?=($items[0]['writer']);?></p>
+    <div id="page" class="container">
+        <div id="content">
+            <div class="title">
+                <h1><?=($items[0]['title']);?></h1>
+            </div>
+            <div class="container">
+                <div id="writer">
+                <p><strong><?=($items[0]['writer']);?></strong></p>
+                </div>
+                <div id="publication_date">
+                <p><?=($items[0]['created_at']);?> </p>
+                </div>
+            </div>
+            <div>
+                <hr class="border">
+                <p><?=($items[0]['body']);?></p>
+            </div>
+        </div>
 <?php
 //var_dump($items);
 ?>
+</div>
 </body>
 </html>
